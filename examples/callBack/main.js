5ef33c448cefbdcc346307af4f85cb01bd9cfc3a
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-10 13:05:27 -0500

    Added files via upload

---
+++ examples/callBack/main.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+
+	$(".slideshow").Slideshow({
+		controls:{
+			prvBtn:"<i class='fa fa-chevron-left'></i>",
+			nextBtn:"<i class='fa fa-chevron-right'></i>",
+			pauseBtn:"<i class='fa fa-pause'></i>",
+			playBtn:"<i class='fa fa-play'></i>",
+			pager:{
+				class:{
+					active:"fa-circle",
+					hidden:"fa-circle-o"
+				},
+				elm:"<i class='fa'></i>"
+			},
+			selectorType:"class",//could also be id or elm
+			selector:"control-panel"
+		},
+		playInterval:2000,
+		animateSpeed:"fast",
+		onload_play:true,
+		onNextSlide:function(){
+			console.log("next slide");
+		},
+		onPreviousSlide:function(){
+			console.log("prev slide");
+		},
+		onSlideChange:function(){
+			console.log("slide change");
+		},
+		onSlideJump:function(){
+			console.log("slide jump to");
+		}
+	});
+	
+});
