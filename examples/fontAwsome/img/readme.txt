b24d1cd893302a77daf9364ec2a41e52f25d9742
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-10 13:07:51 -0500

    Added files via upload

---
+++ examples/fontAwsome/img/readme.txt
@@ -0,0 +1,1 @@
+The images in this example are random photos from google and may change in future updates. 
---
+++ examples/fontAwsome/main.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+	$(".slideshow").Slideshow({
+		controls:{
+			prvBtn:"<i class='fa fa-chevron-left'></i>",
+			nextBtn:"<i class='fa fa-chevron-right'></i>",
+			pauseBtn:"<i class='fa fa-pause'></i>",
+			playBtn:"<i class='fa fa-play'></i>",
+			pager:{
+				class:{
+					active:"fa-circle",
+					hidden:"fa-circle-o"
+				},
+				elm:"<i class='fa'></i>"
+			},
+			selectorType:"class",//could also be id or elm
+			selector:"control-panel"
+		},
+		slides:{
+			selectorType:"elm",
+			selector:"a"
+		},
+		playInterval:5000,
+		onload_play:true
+	});
+	
+});
