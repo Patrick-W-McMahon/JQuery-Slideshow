d7d62d74e961ea2bff596278579089c04f2d8e1e
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-10 13:04:48 -0500

    Update jquery.slideshow.js
    replaced custom settings code with JQuery settings code.
    updated and added in a bunch of new configurable settings.
    many functions that should have been private are now private classes.
    wrapped the plugin into a closure so that it can work when JQuery is not using $ for multi lib comparability.
    removed a lot of old code.

--- jquery.slideshow.js
+++ jquery.slideshow.js
@@ -1,113 +1,145 @@
-$.fn.Slideshow = function(args){
-	var self = this;
-	this.currentSlide = 0;
-	var playState=true;
-	var slides = getSlides(this,args['slides']['selectorType'],args['slides']['selector']);
-	var controls = buildControls(this, args);
-	var anSpeed = "slow";
-	if(isObj(args['animationSpeed'])){
-		anSpeed=args['animationSpeed'];
-	}
-	autoPlay();
-	
-	function buildControls(sliderShow, args){
-		if(isObj(args["controls"])){
-			var ac = args['controls'],cnt = $("."+ac['selector']);
-			var prvBtn = $(ac['prvBtn']).appendTo(cnt);
-			prvBtn.click(function(){
-				self.prev()
-			});
-			var nextBtn = $(ac['nextBtn']).appendTo(cnt);
-			nextBtn.click(function(){
-				self.next();
-			});
-			var paganator = getPaganator(cnt);
-			slides.each(function(index){
-				var typeT = "hidden";
-				if(index==0){
-					typeT = "active";
-				}
-				var acp = ac['pager'],pager = $(acp['elm']).appendTo(paganator);
-				pager.addClass(acp["class"][typeT]);
-				pager.attr("index",index);
-				pager.click(function(){
-					setTo($(this).attr("index"));
+(function ( $ ) {
+	var currentSlide = 0;
+	var state=true;
+	$.fn.Slideshow = function(args){
+		var self = this;
+		var defaults={
+			onNextSlide:function(){},
+			onPreviousSlide:function(){},
+			onSlideChange:function(){},
+			onSlideJump:function(){},
+			animateSpeed: "slow",
+			onload_play:true,
+			playInterval:8000,
+			slides:{
+				selectorType:"elm",
+				selector:"a"
+			},
+			controls:{
+				prvBtn:"<i class='fa fa-chevron-left'></i>",
+				nextBtn:"<i class='fa fa-chevron-right'></i>",
+				pauseBtn:"<i class='fa fa-pause'></i>",
+				playBtn:"<i class='fa fa-play'></i>",
+				pager:{
+					class:{
+						active:"fa-circle",
+						hidden:"fa-circle-o"
+					},
+					elm:"<i class='fa'></i>"
+				},
+				selectorType:"class",
+				selector:"control-panel"
+			},
+		}
+		var settings = $.extend( {}, defaults, args );
+		var slides = getSlides(this,settings.slides.selectorType,settings.slides.selector);
+		var controls = buildControls(this, args);
+		this.start = function(){
+			state=true;
+			loop(settings.playInterval);
+		}
+		
+		this.stop = function(){
+			state=false;
+		}
+		
+		if(settings.onload_play){
+			self.start();
+		}
+		
+		function buildControls(sliderShow, args){
+			if(settings.controls){
+				var ac = settings.controls,cnt = $("."+settings.controls.selector);
+				var prvBtn = $(ac.prvBtn).appendTo(cnt);
+				prvBtn.click(function(){
+					self.prev()
 				});
-			});
-			return cnt;
-		}
-		return undefined;
-	}
-	
-	function autoPlay(){
-		if(isObj(args['playInterval'])){
-			play(args['playInterval']);
-		}else{
-			play(8000);
-		}
-	}
-	
-	function setTo(i){
-		changePager(i);
-		changeSlide(i);
-		self.currentSlide=parseInt(i);
-	}
-	
-	function play(interval){
-		self.next();
-		setTimeout(function(){
-			play(interval);
-		}, interval);
-	}
-	
-	this.next = function(){
-		var sn=parseInt(self.currentSlide)+1;
-		if(sn>slides.length-1){
-			sn=0;
-		}
-		setTo(sn);
-	}
-	
-	this.prev = function(){
-		var s=parseInt(self.currentSlide)-1;
-		if(s<0){
-			s=slides.length-1;
-		}
-		setTo(s);		
-	}
-	
-	function changeSlide(selected){
-		if(self.currentSlide != selected){
-			$(slides.get(self.currentSlide)).fadeOut(anSpeed);
-			$(slides.get(selected)).fadeIn(anSpeed);	
-		}
-	}
-	
-	function changePager(selected){
-		var pagers = controls.children("nav").children("i");
-		var cpc = args['controls']['pager']['class'];
-		pagers.each(function(){
-			if($(this).hasClass(cpc["active"])){
-				self.currentSlide = $(this).attr("index");
-				$(this).removeClass(cpc['active']).addClass(cpc['hidden']);
+				var nextBtn = $(ac.nextBtn).appendTo(cnt);
+				nextBtn.click(function(){
+					self.next();
+				});
+				var paganator = getPaganator(cnt);
+				slides.each(function(index){
+					var typeT = "hidden";
+					if(index==0){
+						typeT = "active";
+					}
+					var acp = ac.pager,pager = $(acp.elm).appendTo(paganator);
+					pager.addClass(acp.class[typeT]);
+					pager.attr("index",index);
+					pager.click(function(){
+						var si = $(this).attr("index");
+						self.setTo(si);
+						settings.onSlideJump.call(self.getSlide(si));
+					});
+				});
+				return cnt;
 			}
-		});
-		$(pagers.get(selected)).removeClass(cpc['hidden']).addClass(cpc['active']);
-	}
+			return undefined;
+		}
+		
+		function loop(interval){
+			if(state){
+				self.next();
+				setTimeout(function(){
+					loop(interval);
+				}, interval);
+			}
+		}
+		
+		this.setTo = function(i){
+			var pagers = controls.children("nav").children("i");
+			var cpc = settings.controls.pager.class;
+			pagers.each(function(){
+				if($(this).hasClass(cpc.active)){
+					currentSlide = $(this).attr("index");
+					$(this).removeClass(cpc.active).addClass(cpc.hidden);
+				}
+			});
+			$(pagers.get(i)).removeClass(cpc.hidden).addClass(cpc.active);
+			if(currentSlide != i){
+				$(slides.get(currentSlide)).fadeOut(settings.animateSpeed);
+				$(slides.get(i)).fadeIn(settings.animateSpeed);	
+			}
+			currentSlide=parseInt(i);
+			settings.onSlideChange.call(this.getSlide(i));
+		}
+		
+		this.getSlide = function(i){
+			if(i<0||i>slides.length){
+				return undefined;
+			}
+			return slides.get(i)
+		}
+		
+		this.next = function(){
+			var s=parseInt(currentSlide)+1;
+			if(s>slides.length-1){
+				s=0;
+			}
+			self.setTo(s);
+			settings.onNextSlide.call(this.getSlide(s));
+		}
+		
+		this.prev = function(){
+			var s=parseInt(currentSlide)-1;
+			if(s<0){
+				s=slides.length-1;
+			}
+			self.setTo(s);
+			settings.onPreviousSlide.call(this.getSlide(s));
+		}
+		
+		function getSlides(slideShow,selType,sel){
+			return slideShow.children(sel);
+		}
+		return this;
+	};
 	
 	function getPaganator(controls_element){
 		var p = document.createElement('nav');
 		controls_element.append(p);
 		return $(p);
-	}
-	
-	function isObj(t){
-		return typeof(t)!==undefined;
-	}
-	
-	function getSlides(slideShow,selType,sel){
-		var results;
-		results = slideShow.children(sel);
-		return results;
-	}
-};
+	}	
+		
+}( jQuery ));
